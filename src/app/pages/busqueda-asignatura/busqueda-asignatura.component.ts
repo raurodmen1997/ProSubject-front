5175fffea5f1a555a875381686c5d2e01b02f298
Raul <raulrodriguezmendez1997@gmail.com>
2020-03-09 09:54:26 +0100

    Realizado servicios de curso, grado, espacio, asignatura

--- src/app/pages/busqueda-asignatura/busqueda-asignatura.component.ts
+++ src/app/pages/busqueda-asignatura/busqueda-asignatura.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Form, FormGroup, FormControl } from '@angular/forms';
+import { BusquedaAsignaturaService } from 'src/app/services/busqueda-asignatura/busqueda-asignatura.service';
+import { GradoService } from 'src/app/services/services.index';
 
 @Component({
   selector: 'app-busqueda-asignatura',
@@ -9,20 +11,42 @@ import { FormBuilder, Form, FormGroup, FormControl } from '@angular/forms';
 export class BusquedaAsignaturaComponent implements OnInit {
 
   form:FormGroup;
+  universidades:any[];
 
-  constructor(private fb:FormBuilder) { }
+  grados:any[];
+
+  constructor(private fb:FormBuilder, private busquedaAsignaturaService:BusquedaAsignaturaService, private gradoService:GradoService) { }
 
   lista:any[] = ['hola', 'adios', 'buenas', 'tardes'];
 
   ngOnInit() {
+    this.inicializarFormulario();
+    this.busquedaAsignaturaService.getUniversidades().subscribe(data=>{
+      this.universidades = data;
+      console.log(this.universidades);
 
+      this.gradoService.getGrados().subscribe(data=>{
+        this.grados = data;
+        console.log(this.grados);
+      });
+
+
+
+    });
+
+
+    
+
+  }
+
+
+  inicializarFormulario(){
     this.form = this.fb.group({
       universidad: new FormControl(''),
       grado: new FormControl(''),
       curso: new FormControl(''),
       asignatura: new FormControl('')
     });
-
   }
 
 
